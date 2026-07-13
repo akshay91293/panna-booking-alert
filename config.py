4d5d2d61ef5a61f701479f3f7b46faf57fdb1437
akshay91293 <42751274+akshay91293@users.noreply.github.com>
2026-07-13 14:56:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+PARK_NAME = "Panna Tiger Reserve"
+
+PARK_ID = 3
+
+URL = f"https://forest.mponline.gov.in/Search.aspx?park={PARK_ID}"
+
+EXPECTED_TO_DT = "9/30/2026 11:59:59 PM"
+
+TELEGRAM_ENABLED = True
