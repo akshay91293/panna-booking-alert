febbb9b8f39c08f162229b90fb915a408c25d1c9
akshay91293 <42751274+akshay91293@users.noreply.github.com>
2026-07-13 15:05:17 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,47 @@
 from bs4 import BeautifulSoup
 
 
-def get_to_dt(html: str) -> str:
+def parse(html: str):
+
     soup = BeautifulSoup(html, "lxml")
 
-    field = soup.find(id="To_dt")
+    data = {}
 
-    if field is None:
-        raise Exception("Could not locate To_dt.")
+    # Backend season end date
+    to_dt = soup.find(id="To_dt")
+    data["to_dt"] = to_dt["value"] if to_dt else None
 
-    return field["value"]
+    # Park name
+    park = soup.find("div", id="ParkHeading")
+    data["park_name"] = park.get_text(strip=True) if park else None
+
+    # Rule image
+    rule_img = soup.find("img", src=lambda s: s and "ForestNewRule" in s)
+    data["rule_image"] = rule_img["src"] if rule_img else None
+
+    # Single seat banner
+    single = soup.find(id="SingleSeatlink")
+    data["single_seat_banner"] = (
+        single.get_text(" ", strip=True)
+        if single else None
+    )
+
+    # Tatkal banner
+    tatkal = soup.find(id="TatkalLink")
+    data["tatkal_banner"] = (
+        tatkal.get_text(" ", strip=True)
+        if tatkal else None
+    )
+
+    # Entire Important Notes block
+    notes = soup.select_one(".BlockContent")
+
+    if notes:
+        data["important_notes"] = notes.get_text(
+            "\n",
+            strip=True
+        )
+    else:
+        data["important_notes"] = None
+
+    return data
