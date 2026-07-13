b55efb96f6d9cdfe070de056dfc495e66ab565de
akshay91293 <42751274+akshay91293@users.noreply.github.com>
2026-07-13 14:15:27 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,15 +1,20 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-
     browser = p.chromium.launch(headless=True)
 
-    page = browser.new_page()
+    page = browser.new_page(
+        viewport={"width": 1600, "height": 900}
+    )
 
-    page.goto("https://forest.mponline.gov.in/Search.aspx?park=3")
+    page.goto(
+        "https://forest.mponline.gov.in/Search.aspx?park=3",
+        wait_until="networkidle"
+    )
 
     page.screenshot(path="screenshot.png", full_page=True)
 
-    print(page.title())
+    with open("page.html", "w", encoding="utf-8") as f:
+        f.write(page.content())
 
     browser.close()
